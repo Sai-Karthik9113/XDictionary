84f83eb12b511bae28abbabb1ef14cdc19f01678
Sai-Karthik9113 <sai9898karthik@gmail.com>
2024-11-02 15:09:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { WordsInDictionary } from './WordsLibrary';
 
 function App() {
+  const [searchKeyWord, setSearchKeyWord] = useState('');
+  const [keyWordMeaning, setKeyWordMeaning] = useState('');
+
+  const handleChange = (e) => {
+    setSearchKeyWord(e.target.value);
+  }
+
+  const handleClick = () => {
+    WordsInDictionary.find((item) => {
+      if (searchKeyWord.toLocaleLowerCase() === item.word.toLocaleLowerCase()) {
+        setKeyWordMeaning(item.meaning);
+      } else {
+        setKeyWordMeaning('Word not found in the dictionary.');
+      }
+    });
+  }
+
   return (
     <div className="App">
-
+      <h1>Dictionary App</h1>
+      <input value={searchKeyWord} onChange={handleChange} placeholder='Search for a word...' />
+      <button onClick={handleClick}>
+        Search
+      </button>
+      <h4>Definition:</h4>
+      {keyWordMeaning && <p>{keyWordMeaning}</p>}
     </div>
   );
 }
